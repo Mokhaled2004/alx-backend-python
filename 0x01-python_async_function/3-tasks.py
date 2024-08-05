15647160225a2c3751b636229eaa1b41a69c4b65
Mokhaled2004 <mokhaled2004@gmail.com>
2024-08-05 19:58:33 +0000

    Solo Leveling

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-'''Task 3's module.
-'''
+"""Create a task"""
 import asyncio
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    '''Creates an asynchronous task for wait_random.
-    '''
+    """Return a task"""
     return asyncio.create_task(wait_random(max_delay))
+
