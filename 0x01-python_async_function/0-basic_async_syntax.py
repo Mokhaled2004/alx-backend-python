b6250c2ec3ec2ca5ab7593955ced374b2d21e2e2
Mokhaled2004 <mokhaled2004@gmail.com>
2024-08-05 19:55:50 +0000

    Solo Leveling

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,11 @@
-
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""Defines an asynchronous coroutine"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds.
-    '''
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
-
+    """Waits for a random delay between 0 and max_delay"""
+    random_number = random.uniform(0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
