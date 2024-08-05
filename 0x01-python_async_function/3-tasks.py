56f3bb5cc8073a08cab2e41ce34998e7164f0acb
Mokhaled2004 <mokhaled2004@gmail.com>
2024-08-05 20:02:08 +0000

    Solo Leveling

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-
+"""
+A module for creating asyncio Tasks that run asynchronous functions.
+"""
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-task_wait_random = __import__('3-tasks').task_wait_random
 
-
-async def test(max_delay: int) -> float:
-    task = task_wait_random(max_delay)
-    await task
-    print(task.__class__)
-
-asyncio.run(test(5))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create an asyncio Task that waits for a random amount of time
+    up to `max_delay` seconds when run.
+    Returns the created Task object.
+    """
+    return asyncio.create_task(wait_random(max_delay))
 
