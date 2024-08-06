7c9328fff91d7b244903f84a348b5fe956483914
Mokhaled2004 <mokhaled2004@gmail.com>
2024-08-06 19:24:02 +0000

    Solo Leveling

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,11 @@
 
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
+"""Generates a list from an async comprehension"""
 from typing import List
-from importlib import import_module as using
-
-
-async_generator = using('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
-    '''
-    return [num async for num in async_generator()]
+    """Collects async generated list and return it"""
+    return [_ async for _ in async_generator()]
 
